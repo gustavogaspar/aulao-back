822d1246f4a37ad94cab78aa1a697ed7090aeca3
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-11-04 14:54:18 -0300

    first-commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+const mysql = require('mysql'); 
+const bodyParser = require('body-parser')
+
+
+app.use(cors({
+    origin: '*',
+    methods: ['POST']
+}));
+
+app.use(bodyParser.json())
+
+app.post('/api', (req, res) => {
+    console.log(req.body)
+    sendToDB(req.body);
+    res.send("message received")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at ${port}`)
+})
+
+
+function sendToDB(body){
+    const con = mysql.createConnection({
+        host: "arm-mysql.privdb.armvcn.oraclevcn.com",
+        user: "db_user",
+        password: "Oracle@123",
+        database: "student"
+      });
+    
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected to the database!");
+        var sql = `INSERT INTO student (fname, lname, email) VALUES (${body.firstName},${body.lastName},${body.email})`;
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Record inserted, ID: " + result.insertId);
+        });
+      });
+
+}
