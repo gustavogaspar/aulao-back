211b5599af4bae486585340932aa8b6b69e46a5c
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-11-04 15:03:02 -0300

    final-commit

---
+++ db-script/student_db.sql
@@ -0,0 +1,5 @@
+CREATE USER 'db_user'@'%' IDENTIFIED BY "Oracle@123";
+CREATE DATABASE student;
+GRANT ALL ON student.* to 'db_user'@'%';
+USE student;
+CREATE TABLE student (fname VARCHAR(20), lname VARCHAR(20), email VARCHAR(20));
