ea0c61bb9c61cdbeaf556a3915e093d15e3b2d50
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-11-04 16:50:55 -0300

    final

--- db-script/student_db.sql
+++ db-script/student_db.sql
@@ -2,4 +2,4 @@ CREATE USER 'db_user'@'%' IDENTIFIED BY "Oracle@123";
 CREATE DATABASE student;
 GRANT ALL ON student.* to 'db_user'@'%';
 USE student;
-CREATE TABLE student (fname VARCHAR(20), lname VARCHAR(20), email VARCHAR(20));
+CREATE TABLE student (fname VARCHAR(20), lname VARCHAR(20), email VARCHAR(100));
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function sendToDB(body){
       con.connect(function(err) {
         if (err) throw err;
         console.log("Connected to the database!");
-        var sql = `INSERT INTO student (fname, lname, email) VALUES (${body.firstName},${body.lastName},${body.email})`;
+        var sql = `INSERT INTO student (fname, lname, email) VALUES ('${body.firstName}','${body.lastName}','${body.email}')`;
         con.query(sql, function (err, result) {
           if (err) throw err;
           console.log("Record inserted, ID: " + result.insertId);
