01e32df2b013a9e3afeafe4ba5c507d2c2b7e313
GALVES <GALVES@GALVES-7410.br.oracle.com>
2021-11-08 15:56:46 -0300

    zipkin-instrumentation

--- index.js
+++ index.js
@@ -2,44 +2,63 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3000
-const mysql = require('mysql'); 
+const mysql2 = require('mysql2');
 const bodyParser = require('body-parser')
 
+// APM Configuration
+const apm_upload_url = 'https://aaaac7hrlrc7saaaaaaaaaayre.apm-agt.us-ashburn-1.oci.oraclecloud.com/20200101/observations/public-span?dataFormat=zipkin&dataFormatVersion=2&dataKey=DIZBCJU6DBOIPEGP5IFGBYJ4U4RWHQFW';
+const serviceName = 'setStudent';
+
+//Zipkin Config
+
+const { Tracer, ExplicitContext, BatchRecorder, jsonEncoder } = require("zipkin");
+const { HttpLogger } = require("zipkin-transport-http");
+const zipkinMiddleware = require("zipkin-instrumentation-express").expressMiddleware;
+const zipkinMySQL = require('zipkin-instrumentation-mysql2');
+
+const tracer = new Tracer({
+  ctxImpl: new ExplicitContext(),
+  recorder: new BatchRecorder({
+    logger: new HttpLogger({
+      endpoint: apm_upload_url,
+      jsonEncoder: jsonEncoder.JSON_V2,
+    }),
+  }),
+  localServiceName: serviceName,
+});
+
+app.use(zipkinMiddleware({ tracer }));
 
 app.use(cors({
-    origin: '*',
-    methods: ['POST']
+  origin: '*',
+  methods: ['POST']
 }));
 
 app.use(bodyParser.json())
 
 app.post('/api', (req, res) => {
-    console.log(req.body)
-    sendToDB(req.body);
-    res.send("message received")
+  console.log(req.body)
+  sendToDB(req.body, tracer);
+  res.send("message received")
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at ${port}`)
+  console.log(`Example app listening at ${port}`)
 })
 
 
-function sendToDB(body){
-    const con = mysql.createConnection({
-        host: "<YOUR_DB_FQDN>",
-        user: "db_user",
-        password: "Oracle@123",
-        database: "student"
-      });
-    
-      con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected to the database!");
-        var sql = `INSERT INTO student (fname, lname, email) VALUES ('${body.firstName}','${body.lastName}','${body.email}')`;
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("Record inserted, ID: " + result.insertId);
-        });
-      });
+function sendToDB(body, tracer) {
 
+  const wrappedMysqlClient = zipkinMySQL(mysql2, tracer)
+  const connOptions = {
+    host: "<YOUR_DB_FQDN>",
+    user: "db_user",
+    password: "Oracle@123",
+    database: "student"
+  }
+  const client = wrappedMysqlClient.createConnection(connOptions)
+
+  client.query(`INSERT INTO student (fname, lname, email) VALUES ('${body.firstName}','${body.lastName}','${body.email}')`,(err, result) => {
+    console.log(err, result)
+  })
 }
